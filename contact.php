c2993c92a80eabb05238203c4e617708fd9c8cfd
Taylor Williams <68873291+taylor-williams1999@users.noreply.github.com>
2020-07-28 11:18:24 -0400

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,9 +1,10 @@
 <?php
 if($_POST["message"]) {
-    mail("taylor.williams1999@outlook.com", $_POST["subject"], $_POST["message"], "$_POST["email"], $_POST["name"]);
+    mail("taylor.williams1999@outlook.com", $_POST["subject"], $_POST["message"], "$_POST["email"] . $_POST["name"]);
 }
 
 if($_POST["Cmessage"]) {
-    mail("taylor.williams1999@outlook.com", $_POST["Csubject"], $_POST["Cmessage"], $_POST["Cemail"], $_POST["Cname"]);
+    $headers = "From: $_POST[Cemail]" . $_POST["Cname"];
+    mail("taylor.williams1999@outlook.com", $_POST["Csubject"], $_POST["Cmessage"], $headers);
 }
 ?>
